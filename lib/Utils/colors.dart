ccbccd7629c09fbf7e5c530f4bc2280c01a83ec9
Joker223311 <yaakali33@gmail.com>
2024-05-22 21:25:27 +0700

    Initiat

---
+++ lib/Utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor1 = const Color(0xffE9EAF7);
+Color backgroundColor2 = const Color(0xffF4EEF2);
+Color backgroundColor3 = const Color(0xffEBEBF2);
+Color backgroundColor4 = const Color(0xffE3EDF5);
+Color primaryColor = Color.fromARGB(255, 205, 232, 229);
+Color textColor1 = const Color(0xff353047);
+Color textColor2 = const Color(0xff6F6B7A);
+Color buttonColor = Color.fromARGB(255, 36, 132, 228);
